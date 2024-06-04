7aba9afee6a58c2ce3d1b7d42a8db60094d85ca1
zll <ahjszll@126.com>
2024-06-04 21:01:01 +0800

    移动

--- DICOMViewApp/UserControls/DICOMView/UCSingleView.axaml.cs
+++ DICOMViewApp/UserControls/DICOMView/UCSingleView.axaml.cs
@@ -2,40 +2,75 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using DICOMViewApp.UserControls.DICOMView.ViewModels;
+using ReactiveUI;
+using System.Diagnostics;
 
 namespace DICOMViewApp.UserControls.DICOMView;
 
 public partial class UcSingleView : UserControl
 {
-    private bool _pressed = false;
+    private Point? _mouseDownPos = null;
+    private Point? _imgOldPoint = null;
     private UcSingleViewModel _viewModel = new UcSingleViewModel();
+    private EnumOperationMode _mode = EnumOperationMode.None;
 
 
     public UcSingleView()
     {
         InitializeComponent();
+        this.Loaded += UcSingleView_Loaded;
         imgParent.PointerMoved += ImgParent_PointerMoved;
         imgParent.PointerPressed += ImgParent_PointerPressed;
         imgParent.PointerReleased += ImgParent_PointerReleased;
+        _viewModel.PropertyChanged += _viewModel_PropertyChanged;
         this.DataContext = _viewModel;
+    }
+
+    private void _viewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        switch (_viewModel.OperationMode) 
+        {
+            case EnumOperationMode.None: imgParent.Cursor = Cursor.Default; break;
+            case EnumOperationMode.Move: imgParent.Cursor = new Cursor(StandardCursorType.Hand); break;
+            case EnumOperationMode.Scale: imgParent.Cursor = new Cursor(StandardCursorType.DragMove); break;
+            case EnumOperationMode.Rotate: imgParent.Cursor = new Cursor(StandardCursorType.Arrow); break;
+        }
+    }
+
+    private void UcSingleView_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
     }
 
     private void ImgParent_PointerReleased(object? sender, PointerReleasedEventArgs e)
     {
-        _pressed = false;
+        _mouseDownPos = null;
     }
 
     private void ImgParent_PointerPressed(object? sender, PointerPressedEventArgs e)
     {
-        _pressed = true;
+        var point = e.GetCurrentPoint(sender as Control);
+        if (point.Properties.IsLeftButtonPressed)
+        {
+            _mouseDownPos = point.Position;
+            _imgOldPoint = new Point(_viewModel.XX, _viewModel.YY);
+        }
     }
 
     private void ImgParent_PointerMoved(object? sender, PointerEventArgs e)
     {
-        if (_pressed) 
+        var point = e.GetCurrentPoint(sender as Control);
+        if (point.Properties.IsLeftButtonPressed)
         {
-            _viewModel.XX = (int)e.GetPosition(imgParent).X;
-            _viewModel.YY = (int)e.GetPosition(imgParent).Y;
+            if (_mouseDownPos != null && _imgOldPoint != null)
+            {
+                var newPos = point.Position;
+                var dis = (_mouseDownPos - newPos);
+                _viewModel.XX = _imgOldPoint.Value.X - dis.Value.X;
+                _viewModel.YY = _imgOldPoint.Value.Y - dis.Value.Y;
+                Debug.WriteLine(dis.Value);
+            }
         }
     }
+
+    
 }
--- DICOMViewApp/UserControls/DICOMView/ViewModels/UcSingleViewModel.cs
+++ DICOMViewApp/UserControls/DICOMView/ViewModels/UcSingleViewModel.cs
@@ -4,18 +4,37 @@ namespace DICOMViewApp.UserControls.DICOMView.ViewModels
 {
     public class UcSingleViewModel : ReactiveObject
     {
-        private int _xx =0;
-        public int XX
+        private double _xx = 0;
+        public double XX
         {
             get => _xx;
             set => this.RaiseAndSetIfChanged(ref _xx, value);
         }
 
-        private int _yy = 0;
-        public int YY
+        private double _yy = 0;
+        public double YY
         {
             get => _yy;
             set => this.RaiseAndSetIfChanged(ref _yy, value);
         }
+
+        private EnumOperationMode _operationMode = EnumOperationMode.None;
+        public EnumOperationMode OperationMode
+        {
+            get => _operationMode;
+            set => this.RaiseAndSetIfChanged(ref _operationMode, value);
+        }
+
+        public void ChangeMode(EnumOperationMode operationMode)
+        {
+            OperationMode = operationMode;
+        }
+    }
+    public enum EnumOperationMode
+    {
+        None,
+        Move,
+        Scale,
+        Rotate
     }
 }
